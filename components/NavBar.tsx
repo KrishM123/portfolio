ebbbedeea4692255ffec58196e560faf8ed994c6
KrishM123 <kahnscatterkrish@gmail.com>
2024-09-28 23:01:03 -0400

    responsiveness

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
                         <span className="cursor-blink">▮</span>
                     </Link>
                     <div className="hidden lg:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
+                        <div className="ml-10 flex items-baseline space-x-4 justify-end">
                             <Link href="/my-work" className="hover:text-dark">
                                 [ my work ]
                             </Link>
@@ -29,27 +29,31 @@ export default function NavBar() {
                     </div>
                     <div className="lg:hidden">
                         <button onClick={toggleExpandNav} className="inline-flex items-center justify-center p-2 rounded-md text-darkest hover:text-dark focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                            <span className="sr-only">Open main menu</span>
-                            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
+                            <span className="sr-only">Toggle main menu</span>
+                            {expandNav ? (
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            ) : (
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                                </svg>
+                            )}
                         </button>
                     </div>
                 </div>
             </div>
 
-            {expandNav && (
-                <div className="lg:hidden">
-                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                        <Link href="/my-work" className="hover:text-dark block px-3 py-2 rounded-md text-base font-medium">
-                            [ my work ]
-                        </Link>
-                        <Link href="https://drive.google.com/file/d/1kE9BLv6GWTIGxWWH4V8nFmHc4zO6x0bG/view?usp=sharing" className="hover:text-dark block px-3 py-2 rounded-md text-base font-medium">
-                            [ resume ]
-                        </Link>
-                    </div>
+            <div className={`lg:hidden ${expandNav ? 'block' : 'hidden'} w-full bg-lightest`}>
+                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col items-end">
+                    <Link href="/my-work" className="hover:text-dark block px-3 py-2 rounded-md text-base font-medium">
+                        [ my work ]
+                    </Link>
+                    <Link href="https://drive.google.com/file/d/1kE9BLv6GWTIGxWWH4V8nFmHc4zO6x0bG/view?usp=sharing" className="hover:text-dark block px-3 py-2 rounded-md text-base font-medium">
+                        [ resume ]
+                    </Link>
                 </div>
-            )}
+            </div>
         </nav>
     );
 }
