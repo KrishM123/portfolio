7e257d3b95a2747ed4126897f8674ff278ce3b40
KrishM123 <kahnscatterkrish@gmail.com>
2024-10-03 18:59:38 -0400

    temp links with empty not 404

--- data/caseStudies.ts
+++ data/caseStudies.ts
@@ -17,11 +17,118 @@
             <ProjectBox image={"/images/Mean Reversion.png"} link={"https://github.com/KrishM123/Mean-Reversion"} title={"Mean Reversion"}/>
           </div> */}
 
+// export const caseStudies = [
+//     {
+//         title: "Red Handed: A Slap To Keep You Awake",
+//         description: "Machine vision model on a Raspberry Pi, controlling a slapper arm. Winner of Flow and 2nd for Qualcomm at MakeUofT 2024",
+//         link: "/projects/sage",
+//         file: "sage.md",
+//         tags: ["ML", "Hardware"],
+//         date: "2024-02-26",
+//     },
+//     {
+//         title: "Sage: Optimal Emissions With Policy Matching",
+//         description: "A novel mathematical model to determine optimial emissions, and AI agents to reccommend the policy required.",
+//         link: "/projects/sage",
+//         file: "sage.md",
+//         tags: ["ML", "Mathematical Model"],
+//         date: "2024-06-23",
+//     },
+//     {
+//         title: "Medschart: A Diabetes Patient Management System",
+//         description: "An administrative tool to make pharmacists' lives easier. Acquired by Shoppers Drug Mart.",
+//         link: "/projects/medschart",
+//         file: "medschart.md",
+//         tags: ["Full-Stack"],
+//         date: "2024-07-01",
+//     },
+//     {
+//         title: "One Sheet A Day: Math Drills",
+//         description: "A math practice book and website. Creation fully automated.",
+//         link: "/projects/math-drills",
+//         file: "math-drills.md",
+//         tags: ["Full-Stack"],
+//         date: "2020-09-15",
+//     },
+//     {
+//         title: "Runk: Accurate Product Review Ranking",
+//         description: "Ranked product reviews based on profile similarities. A novel autoencoder architecutre for updating your profile based on the reviews you leave.",
+//         link: "/projects/runk",
+//         file: "runk.md",
+//         tags: ["ML", "Full-Stack"],
+//         date: "2024-09-15",
+//     },
+//     {
+//         title: "No To Photo: Photo Blocking Pendant",
+//         description: "Developed a wearable device that emits a counter-flash to obscure unwanted photos. Gold Medal + Best Junior Physics Project at Canada-Wide Science Fair",
+//         link: "/projects/no-to-photo-photo-blocking-pendant",
+//         file: "no-to-photo-photo-blocking-pendant.md",
+//         tags: ["Research", "Mathematical Model"],
+//         date: "2019-05-01",
+//     },
+//     {
+//         title: "Point of Care Prothrombin Time Test",
+//         description: "Created a portable device for quick and accurate blood coagulation testing, improving accessibility to critical medical diagnostics. Siver Medal at Canada-Wide Science Fair",
+//         link: "/projects/point-of-care-prothrombin-time-test",
+//         file: "point-of-care-prothrombin-time-test.md",
+//         tags: ["Research", "Biotech"],
+//         date: "2021-04-01",
+//     },
+//     {
+//         title: "Pee Xylophone: A Novel Non-Invasive Bladder Pressure Measurement System",
+//         description: "Developed a patent-pending medical device that measures bladder pressure non-invasively, revolutionizing urological diagnostics. Gold Medal + Youth Can Innovate Plus at Canada-Wide Science Fair. Team Canada at International Science and Engineering Fair",
+//         link: "/projects/pee-xylophone-novel-non-invasive-bladder-pressure-measurement-system",
+//         file: "pee-xylophone-novel-non-invasive-bladder-pressure-measurement-system.md",
+//         tags: ["Research", "Mathematical Model", "Biotech"],
+//         date: "2023-04-05",
+//     },
+//     {
+//         title: "Self-Driving Delivery Car",
+//         description: "Created a Raspberry Pi self-driving car to deliver food around campus.",
+//         link: "/projects/self-driving-delivery-car",
+//         file: "self-driving-delivery-car.md",
+//         tags: ["ML", "Hardware"],
+//         date: "2023-12-04",
+//     },
+//     {
+//         title: "DriveSense: Accurate Driver Rating",
+//         description: "Innovatively used machine vision to accurately rate driver performance and promote safer driving habits for insurance discounts. Winner at Hack the North",
+//         link: "/projects/drivesense-accurate-driver-rating",
+//         file: "drivesense-accurate-driver-rating.md",
+//         tags: ["ML", "Mathematical Model", "Full-Stack"],
+//         date: "2023-09-17",
+//     },
+//     {
+//         title: "CogniCare: Alzheimer's Prevention and Detection",
+//         description: "Designed a platform to assist in early detection and prevention of Alzheimer's disease through cognitive assessments and personalized care plans. Winner at MHacks",
+//         link: "/projects/cognicare-alzheimers-prevention-and-detection",
+//         file: "cognicare-alzheimers-prevention-and-detection.md",
+//         tags: ["ML", "Biotech", "Full-Stack"],
+//         date: "2023-11-23",
+//     },
+//     {
+//         title: "MAAPA: Smart CTRL+F",
+//         description: "Developed an advanced search tool that enhances the traditional CTRL+F functionality with intelligent context-aware searching capabilities before ChatGPT.",
+//         link: "/projects/maapa-smart-ctrl-f",
+//         file: "maapa-smart-ctrl-f.md",
+//         tags: ["Full-Stack", "ML"],
+//         date: "2021-09-25",
+//     },
+//     {
+//         title: "Mid-Term Trading Algorithm",
+//         description: "Created a stock prediction algorithm initially based on the mean reversion. Beat the S&P 500 for 2 years.",
+//         link: "/projects/mean-reversion",
+//         file: "mean-reversion.md",
+//         tags: ["Quant", "ML", "Mathematical Model"],
+//         date: "2024-09-29",
+//     }
+// ];
+
 export const caseStudies = [
     {
         title: "Red Handed: A Slap To Keep You Awake",
         description: "Machine vision model on a Raspberry Pi, controlling a slapper arm. Winner of Flow and 2nd for Qualcomm at MakeUofT 2024",
-        link: "/projects/sage",
+        link: "https://devpost.com/software/red-handed",
         file: "sage.md",
         tags: ["ML", "Hardware"],
         date: "2024-02-26",
@@ -29,7 +136,7 @@ export const caseStudies = [
     {
         title: "Sage: Optimal Emissions With Policy Matching",
         description: "A novel mathematical model to determine optimial emissions, and AI agents to reccommend the policy required.",
-        link: "/projects/sage",
+        link: "https://devpost.com/software/sage-5x6ed2",
         file: "sage.md",
         tags: ["ML", "Mathematical Model"],
         date: "2024-06-23",
@@ -45,7 +152,7 @@ export const caseStudies = [
     {
         title: "One Sheet A Day: Math Drills",
         description: "A math practice book and website. Creation fully automated.",
-        link: "/projects/math-drills",
+        link: "https://math-drills-lac.vercel.app/",
         file: "math-drills.md",
         tags: ["Full-Stack"],
         date: "2020-09-15",
@@ -61,7 +168,7 @@ export const caseStudies = [
     {
         title: "No To Photo: Photo Blocking Pendant",
         description: "Developed a wearable device that emits a counter-flash to obscure unwanted photos. Gold Medal + Best Junior Physics Project at Canada-Wide Science Fair",
-        link: "/projects/no-to-photo-photo-blocking-pendant",
+        link: "https://drive.google.com/file/d/14rQa1GPFtaPWgxOX7aG2ZTQNZ_Qt98tj/view?usp=sharing",
         file: "no-to-photo-photo-blocking-pendant.md",
         tags: ["Research", "Mathematical Model"],
         date: "2019-05-01",
@@ -69,7 +176,7 @@ export const caseStudies = [
     {
         title: "Point of Care Prothrombin Time Test",
         description: "Created a portable device for quick and accurate blood coagulation testing, improving accessibility to critical medical diagnostics. Siver Medal at Canada-Wide Science Fair",
-        link: "/projects/point-of-care-prothrombin-time-test",
+        link: "https://drive.google.com/file/d/1-bddRs1d3afIfAdnOOv7y7xKHSWJ5ktX/view?usp=sharing",
         file: "point-of-care-prothrombin-time-test.md",
         tags: ["Research", "Biotech"],
         date: "2021-04-01",
@@ -77,7 +184,7 @@ export const caseStudies = [
     {
         title: "Pee Xylophone: A Novel Non-Invasive Bladder Pressure Measurement System",
         description: "Developed a patent-pending medical device that measures bladder pressure non-invasively, revolutionizing urological diagnostics. Gold Medal + Youth Can Innovate Plus at Canada-Wide Science Fair. Team Canada at International Science and Engineering Fair",
-        link: "/projects/pee-xylophone-novel-non-invasive-bladder-pressure-measurement-system",
+        link: "https://partner.projectboard.world/isef/project/enbm051-non-invasive-bladder-pressure-measurement-system/",
         file: "pee-xylophone-novel-non-invasive-bladder-pressure-measurement-system.md",
         tags: ["Research", "Mathematical Model", "Biotech"],
         date: "2023-04-05",
@@ -85,7 +192,7 @@ export const caseStudies = [
     {
         title: "Self-Driving Delivery Car",
         description: "Created a Raspberry Pi self-driving car to deliver food around campus.",
-        link: "/projects/self-driving-delivery-car",
+        link: "https://github.com/rajansagarwal/self-driving-tank",
         file: "self-driving-delivery-car.md",
         tags: ["ML", "Hardware"],
         date: "2023-12-04",
@@ -93,7 +200,7 @@ export const caseStudies = [
     {
         title: "DriveSense: Accurate Driver Rating",
         description: "Innovatively used machine vision to accurately rate driver performance and promote safer driving habits for insurance discounts. Winner at Hack the North",
-        link: "/projects/drivesense-accurate-driver-rating",
+        link: "https://devpost.com/software/drive-sense",
         file: "drivesense-accurate-driver-rating.md",
         tags: ["ML", "Mathematical Model", "Full-Stack"],
         date: "2023-09-17",
@@ -101,7 +208,7 @@ export const caseStudies = [
     {
         title: "CogniCare: Alzheimer's Prevention and Detection",
         description: "Designed a platform to assist in early detection and prevention of Alzheimer's disease through cognitive assessments and personalized care plans. Winner at MHacks",
-        link: "/projects/cognicare-alzheimers-prevention-and-detection",
+        link: "https://devpost.com/software/cognicare-companion-app-for-memory-support",
         file: "cognicare-alzheimers-prevention-and-detection.md",
         tags: ["ML", "Biotech", "Full-Stack"],
         date: "2023-11-23",
@@ -109,7 +216,7 @@ export const caseStudies = [
     {
         title: "MAAPA: Smart CTRL+F",
         description: "Developed an advanced search tool that enhances the traditional CTRL+F functionality with intelligent context-aware searching capabilities before ChatGPT.",
-        link: "/projects/maapa-smart-ctrl-f",
+        link: "https://github.com/KrishM123/maapa",
         file: "maapa-smart-ctrl-f.md",
         tags: ["Full-Stack", "ML"],
         date: "2021-09-25",
